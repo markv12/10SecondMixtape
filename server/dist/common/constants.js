ddd45a39361db4e1798fa9c805d248e01880b09d
Jasper Stephenson <jasperstephenson@gmail.com>
2022-09-30 10:35:17 +0900

    remove dist files

--- server/dist/common/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseSubdirectory = void 0;
-exports.baseSubdirectory = 'ld51';
-//# sourceMappingURL=constants.js.map
