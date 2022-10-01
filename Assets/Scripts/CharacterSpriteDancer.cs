a940ed88b43af9fcbd10a5667181dc0c5f2da08f
Jasper Stephenson <jasperstephenson@gmail.com>
2022-10-01 19:57:14 +0900

    scene stuff

---
+++ Assets/Scripts/CharacterSpriteDancer.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpriteDancer : MonoBehaviour
+{
+    // get the sprite renderer
+    private SpriteRenderer spriteRenderer;
+    private ParticleSystem particleSystem;
+    // get the original position
+    private Vector3 originalPosition;
+    private Vector3 originalScale;
+    private Vector3 originalRotation;
+    private GameObject transformParent;
+    private int currentRoutines = 0;
+
+    private float jamDirection;
+    private float jamSpeed;
+
+    void Start()
+    {
+        jamDirection = Random.Range(-1.0f, 1.0f);
+        jamSpeed = Random.Range(.2f, .6f);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // get particle system on child
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        originalPosition = transform.position;
+        originalScale = transform.localScale;
+        originalRotation = transform.eulerAngles;
+
+
+        // add a new void parent transform on the character
+        transformParent = new GameObject(transform.name + "CharacterParent");
+
+        if (transform.parent) {
+            transformParent.transform.parent = transform.parent.transform;
+        }
+        // set y to bottom of character
+        transformParent.transform.position = new Vector3(transform.position.x, transform.position.y - spriteRenderer.bounds.size.y / 2, transform.position.z);
+        transformParent.transform.rotation = transform.rotation;
+        transformParent.transform.localScale = transform.localScale;
+        transform.parent = transformParent.transform;
+
+        StartCoroutine(RockRoutine());
+    }
+
+    // method to bounce the sprite up and down
+    public void Bounce()
+    {
+        StartCoroutine(RockRoutine());
+    }
+
+    // coroutine to bounce the sprite up and down smoothly
+    private IEnumerator RockRoutine(
+    )
+    {
+        currentRoutines++;
+        particleSystem.Emit(1);
+        float intensity = .05f;
+        // move the sprite up then down in a cos motion
+        for (float t = 0; t < jamSpeed; t += Time.deltaTime)
+        {
+            transformParent.transform.rotation = Quaternion.Euler(
+              originalRotation.x,
+              originalRotation.y,
+              originalRotation.z - jamDirection * Mathf.Cos((t/jamSpeed) * Mathf.PI - Mathf.PI / 2) * intensity * 30
+            );
+
+            transformParent.transform.localScale = new Vector3(
+              originalScale.x,
+              Mathf.Max(
+                  originalScale.y - 1 * Mathf.Cos((t / jamSpeed) * Mathf.PI - Mathf.PI / 2) * intensity, 
+                  originalScale.y - 1 * Mathf.Cos(.5f * Mathf.PI - Mathf.PI / 2) * intensity
+              ),
+              originalScale.z
+            );
+
+            yield return null;
+        }
+
+        currentRoutines--;
+        if (currentRoutines == 0)
+        {
+            // reset the sprite to the original position
+            transformParent.transform.rotation = Quaternion.Euler(originalRotation);
+            transformParent.transform.localScale = originalScale;
+            jamDirection = Random.Range(-1.0f, 1.0f);
+            jamSpeed = Random.Range(.2f, .6f);
+        }
+    }
+}
