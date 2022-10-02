615be721f1a4816e609a17f82a2693442065ad8e
Jasper Stephenson <jasperstephenson@gmail.com>
2022-10-02 11:54:18 +0900

    members and sfx

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -21,47 +21,45 @@ public class AudioManager : MonoBehaviour {
     private int audioSourceIndex = 0;
 
     public AudioClip success;
-    public AudioClip failure;
-    public AudioClip startDrawing;
-    public AudioClip[] brushes;
-    public AudioClip talk;
-    public AudioClip paperFlip;
-    public AudioClip save;
+    public AudioClip plasticClick;
+    public AudioClip crowdMurmur;
+    public AudioClip applause;
 
     public void PlaySuccessSound(float intensity) {
-        PlaySFX(success, 1.0f * intensity);
+        PlaySFX(success, 0.5f * intensity);
     }
 
-    public void PlayFailureSound(float intensity) {
-        PlaySFX(failure, 1.0f * intensity);
+    public void PlayPlasticClickSound(float intensity) {
+        PlaySFX(plasticClick, 1.0f * intensity);
     }
 
-    public void PlaySaveSound(float intensity) {
-        PlaySFX(save, 1.0f * intensity);
+    public void PlayApplauseSound(float intensity) {
+        PlaySFX(applause, 0.8f * intensity);
     }
 
-    public void PlayPaperFlip(float intensity) {
-        PlaySFX(paperFlip, 1.4f * intensity);
+    public void StartCrowdMurmur(float intensity) {
+        PlaySFX(crowdMurmur, 1.0f * intensity, 1, true);
+    }
+    public void StopCrowdMurmur() {
+        StopSFX(crowdMurmur);
     }
 
-    public void PlayStartDrawingSound(float intensity) {
-        PlaySFX(startDrawing, 1.4f * intensity);
-    }
-
-    public void PlayBrushSound(float intensity) {
-        AudioClip sound = brushes[Random.Range(0, brushes.Length)];
-        PlaySFX(sound, 1.0f * intensity);
-    }
-
-    public void PlayTalkSound(float intensity, float pitchCenter) {
-        PlaySFX(talk, 1.0f * Random.Range(0.5f, 1.5f) * intensity, Random.Range(0.6f, 1.4f) * pitchCenter);
-    }
-
-    public void PlaySFX(AudioClip clip, float volume, float pitch = 1) {
+    public void PlaySFX(AudioClip clip, float volume, float pitch = 1, bool loop = false) {
         AudioSource source = GetNextAudioSource();
         source.volume = volume;
         source.pitch = pitch;
+        source.loop = loop;
         source.PlayOneShot(clip);
+        // log state of audio source
+        Debug.Log(source.name + " is playing " + clip.name + " at volume " + volume + " and pitch " + pitch + " and looping " + loop);
+    }
+    public void StopSFX(AudioClip clip) {
+        foreach (AudioSource source in audioSources) {
+            if (source.clip == clip) {
+                source.loop = false;
+                source.Stop();
+            }
+        }
     }
 
     private AudioSource GetNextAudioSource() {
