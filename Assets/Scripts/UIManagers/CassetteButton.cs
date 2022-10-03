580493a7096daf539737115606d17b27feae0dcb
Scott Steffes <swsteffes@gmail.com>
2022-10-02 19:19:24 -0500

    Show text on cassette

--- Assets/Scripts/UIManagers/CassetteButton.cs
+++ Assets/Scripts/UIManagers/CassetteButton.cs
@@ -1,18 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CassetteButton : MonoBehaviour {
     public Image cassetteImage;
-    public InstrumentTrack part;
-    public Image selectedImage;
+    public TMP_Text nameText;
+    public TMP_Text instrumentText;
+    private InstrumentTrack part;
+    public GameObject selectedImage;
 
     public void ShowTrack(InstrumentTrack instrumentTrack) {
         part = instrumentTrack;
         BandMember bandMember = BandMemberMasterList.Instance.GetBandMemberForId(instrumentTrack.instrument);
         cassetteImage.sprite = bandMember.cassetteSprite;
-        selectedImage.enabled = false;
+        nameText.text = instrumentTrack.name;
+        instrumentText.text = bandMember.instrumentDisplayName;
+        selectedImage.SetActive(false);
+
+        nameText.color = bandMember.cassetteTextWhite ? Color.white : Color.black;
+        instrumentText.color = bandMember.cassetteTextWhite ? Color.white : Color.black;
     }
 }
