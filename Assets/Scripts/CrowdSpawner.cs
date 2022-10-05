6b247e9d0385434547b0a234a6b573a28070284b
Jasper Stephenson <jasperstephenson@gmail.com>
2022-10-05 09:06:16 +0900

    crowd spawns properly on window resize

--- Assets/Scripts/CrowdSpawner.cs
+++ Assets/Scripts/CrowdSpawner.cs
@@ -9,11 +9,21 @@ public class CrowdSpawner : MonoBehaviour
     public Sprite[] crowdSprites;
     public float spawnCount = 30;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private int screenWidth;
+
+    private IEnumerator SpawnCrowd() {
+        yield return null;
+
+        screenWidth = Screen.width;
+
+        // wipe all children
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         float maxWidth = transform.GetComponent<RectTransform>().rect.width / 2;
-        float maxHeight = transform.GetComponent<RectTransform>().rect.height / 2;
+        float maxHeight = transform.GetComponent<RectTransform>().rect.height / 2;        
 
         for (int i = 0; i < spawnCount; i++) {
             // create new image and attach it
@@ -34,12 +44,14 @@ public class CrowdSpawner : MonoBehaviour
 
             newImage.AddComponent<CrowdAnimator>();
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+      if (Screen.width != screenWidth) {
+        StartCoroutine(SpawnCrowd());
+      }
         
     }
 }
